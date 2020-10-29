acd9180b27ee205d75beec7016effbdd95e8f5bd
ckandola <kkandola22@hotmail.com>
2020-10-29 16:47:01 -0700

    Add Hangman Component and assets.

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import johnny0 from './johnny_0.png';
+import johnny1 from './johnny_1.png';
+import johnny2 from './johnny_2.png';
+import johnny3 from './johnny_3.png';
+import johnny4 from './johnny_4.png';
+import johnny5 from './johnny_5.png';
+import johnny6 from './johnny_6.png';
+
+const images = [
+    <img src={johnny0} alt={"Johnny Bravo 0"}/>,
+    <img src={johnny1} alt={"Johnny Bravo 1"}/>,
+    <img src={johnny2} alt={"Johnny Bravo 2"}/>,
+    <img src={johnny3} alt={"Johnny Bravo 3"}/>,
+    <img src={johnny4} alt={"Johnny Bravo 4"}/>,
+    <img src={johnny5} alt={"Johnny Bravo 5"}/>,
+    <img src={johnny6} alt={"Johnny Bravo 6"}/>,
+]
+
+export default images;
