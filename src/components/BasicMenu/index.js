066425397193488556391524d709f46aa6e3d77c
ckandola <kkandola22@hotmail.com>
2020-11-01 16:42:42 -0800

    Add BasicMenu and Tooltip components.

---
+++ src/components/BasicMenu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BasicMenu = () => {
+    return (
+        <div>
+            <div><a href='https://www.google.com'>Home</a></div>
+            <div><a href='https://www.google.com'>About</a></div>
+            <div><a href='https://www.google.com'>Services</a></div>
+            <div><a href='https://www.google.com'>Portfolio</a></div>
+            <div><a href='https://www.google.com'>Contact Us</a></div>
+        </div>
+    )
+};
+
+export default BasicMenu;
