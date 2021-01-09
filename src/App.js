17f6bc1459948fca1bd00bf52f601d68725208e3
ckandola <kkandola22@hotmail.com>
2021-01-08 16:10:18 -0800

    Begin Project view.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,122 @@
+// https://dev.to/andyrewlee/how-to-dynamically-render-components-in-react-4n7g
+
 import React from "react";
 import "./App.css";
+import './index.css';
 
-// import FirstComponent from "./components/FirstComponent";
-// import SecondComponent from "./components/SecondComponent";
-// import NamedComponent from "./components/NamedComponent";
+import FirstComponent from "./components/FirstComponent";
+import SecondComponent from "./components/SecondComponent";
+import NamedComponent from "./components/NamedComponent";
 
-// import Tweet from "./components/Tweet";
+import Tweet from "./components/Tweet";
 
-// import Person from "./components/Person";
+import Person from "./components/Person";
 
-// import Pokedex from "./components/Pokedex";
+import Pokedex from "./components/Pokedex";
 
-// import ColorGrid from "./components/ColorGrid";
+import ColorGrid from "./components/ColorGrid";
 
-// import ToDoList from "./components/ToDoList";
+import ToDoList from "./components/ToDoList";
 
-// import LinkApp from "./components/LinkApp";
+import LinkApp from "./components/LinkApp";
 
-// import Jokes from "./components/Jokes";
-//  import Gif from "./components/Gif";
+import Jokes from "./components/Jokes";
+import Gif from "./components/Gif";
 
-// import TicTacToe from "./components/TicTacToe";
+import TicTacToe from "./components/TicTacToe";
 
-//  import HangMan from "./components/HangMan";
+ import HangMan from "./components/HangMan";
 
 //  import Jeopardy from "./components/Jeopardy";
 
-// import Radio from "./components/Radio";
+import Radio from "./components/Radio";
 
-// import BasicMenu from './components/BasicMenu';
+import BasicMenu from './components/BasicMenu';
 
-// import Tooltip from './components/Tooltip';
+import Tooltip from './components/Tooltip';
 
-// import Timer from './components/Timer';
+import Timer from './components/Timer';
 
 // import Slider from './components/Slider';
 
-// import BirthdayReminder from './components/BirthdayReminder';
+import BirthdayReminder from './components/BirthdayReminder';
 
-// import Tours from './components/Tours';
+import Tours from './components/Tours';
 
-// import Reviews from './components/Reviews';
+import Reviews from './components/Reviews';
 
-// import Accordion from './components/Accordion';
-// import accordionData from './components/Accordion/data';
+import Accordion from './components/Accordion';
+import accordionData from './components/Accordion/data';
 
 import FoodMenuProject from "./components/FoodMenu/FoodMenuProject";
 
 class App extends React.Component {
   render() {
+    const componentMapping = {
+      FirstComponent,
+      SecondComponent,
+      NamedComponent,
+      Tweet,
+      Person,
+      Pokedex,
+      ColorGrid,
+      ToDoList,
+      LinkApp,
+      Jokes,
+      Gif,
+      TicTacToe,
+      HangMan,
+      Radio,
+      BasicMenu,
+      Tooltip,
+      Timer,
+      BirthdayReminder,
+      Tours,
+      Reviews,
+      Accordion,
+      FoodMenuProject
+    }
+
+    const componentNames = [
+      'FirstComponent',
+      'SecondComponent',
+      'NamedComponent',
+      'Tweet',
+      'Person',
+      'Pokedex',
+      'ColorGrid',
+      'ToDoList',
+      'LinkApp',
+      'Jokes',
+      'Gif',
+      'TicTacToe',
+      'HangMan',
+      'Radio',
+      'BasicMenu',
+      'Tooltip',
+      'Timer',
+      'BirthdayReminder',
+      'Tours',
+      'Reviews',
+      'Accordion',
+      'FoodMenuProject'
+    ]
+
     return (
       <div>
+        <main>
+          <section>
+            <div className="title">
+              <h2>My Projects</h2>
+              <div className="underline-main"></div>
+            </div>
+            <div className="container">
+              {componentNames.map(componentName => {
+                return <button key={componentName}>{componentName}</button>
+              })}
+            </div>
+          </section>
+        </main>
         {/* <FirstComponent />
         <SecondComponent />
         <NamedComponent style={style} name="CK" /> */}
@@ -134,7 +200,7 @@ class App extends React.Component {
           </div>
         </div> */}
 
-        <FoodMenuProject />
+        {/* <FoodMenuProject /> */}
 
       </div>
     );
