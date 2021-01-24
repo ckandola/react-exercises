97601bb67a5d954642c2c049ccf9cb6d528d731a
ckandola <kkandola22@hotmail.com>
2021-01-24 15:09:18 -0800

    Fix issue with shared div, give all buttons proper links.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import {
   Route,
   Switch,
   Link,
-  useRouteMatch,
-  useParams
 } from "react-router-dom";
 
 import FirstComponent from "./components/FirstComponent";
@@ -54,7 +52,7 @@ import Tours from './components/Tours';
 import Reviews from './components/Reviews';
 
 import Accordion from './components/Accordion';
-// import accordionData from './components/Accordion/data';
+import accordionData from './components/Accordion/data';
 
 import FoodMenuProject from "./components/FoodMenu/FoodMenuProject";
 
@@ -63,30 +61,6 @@ import FoodMenuProject from "./components/FoodMenu/FoodMenuProject";
 // import RouterNested from "./components/RouterBasic/RouterNested";
 
 const App = () => {
-
-  const componentMapping = {
-    "FirstComponent": FirstComponent,
-    "SecondComponent": SecondComponent,
-    "NamedComponent": NamedComponent,
-    "Tweet": Tweet,
-    "Person": Person,
-    "Pokedex": Pokedex,
-    "ColorGrid": ColorGrid,
-    "ToDoList": ToDoList,
-    "Jokes": Jokes,
-    "Gif": Gif,
-    "TicTacToe": TicTacToe,
-    "HangMan": HangMan,
-    "Radio": Radio,
-    "BasicMenu": BasicMenu,
-    "Tooltip": Tooltip,
-    "Timer": Timer,
-    "BirthdayReminder": BirthdayReminder,
-    "Tours": Tours,
-    "Reviews": Reviews,
-    "Accordion": Accordion,
-    "FoodMenuProject": FoodMenuProject
-  }
 
   const componentNames = [
     'FirstComponent',
@@ -123,128 +97,143 @@ const App = () => {
           
           <div>
             <Router>
-              <div className="button-container">
-              {componentNames.map(componentName => {
-                return (
-                  <Link key={componentName} 
-                    className="button-project" 
-                    style={{
-                      color: 'white', 
-                      textDecoration: 'none', 
-                      border: '2px solid darkslateblue',
-                    }}
-                    to={`/${componentName}`} >
-                    {componentName}
-                  </Link>
-                  // <button 
-                  //   key={componentName} 
-                  //   className="button-project" 
-                  // >{componentName}</button>
-                );
-              })}
-              
+              <div>
+                <div className="button-container">
+                  {componentNames.map(componentName => {
+                    return (
+                      <Link 
+                        key={componentName} 
+                        className="button-project" 
+                        style={{
+                          color: 'white', 
+                          textDecoration: 'none', 
+                          border: '2px solid darkslateblue',
+                        }}
+                        to={`/${componentName}`} >
+                        {componentName}
+                      </Link>
+                    );
+                  })}
+                </div>
+                
+                <Switch>
+                  <Route path="/FirstComponent">
+                    <FirstComponent />
+                  </Route>
+                  <Route path="/SecondComponent">
+                    <SecondComponent />
+                  </Route>
+                  <Route path="/NamedComponent">
+                    <NamedComponent name="ckandola"/>
+                  </Route>
+                  <Route path="/Tweet">
+                    <Tweet 
+                      username="chickensrock"
+                      name="ed"
+                      date="6/05/99"
+                      message="Gravy!"/>
+                      <Tweet
+                      username="Mucho Dinero"
+                      name="eddy"
+                      date="7/02/99"
+                      message="Jawbreakers, Ed!"
+                    />
+                    <Tweet
+                      username="my_kingdom_for_suncreen"
+                      name="edd"
+                      date="7/05/99"
+                      message="Messy, messy, messy!"
+                    />
+                  </Route>
+                  <Route path="/Person">
+                    <Person
+                      name="AgeEight"
+                      age="8"
+                      hobbies={["running, schooling, eating"]}
+                    />
+                    <Person
+                      name="TooLongOfAName"
+                      age="20"
+                      hobbies={["thinking, pondering, querying, imagining"]}
+                    />
+                    <Person name="Bob" age="30" hobbies={["building, fixing it"]} />
+                  </Route>
+                  <Route path="/Pokedex">
+                    <Pokedex />
+                  </Route>
+                  <Route path="/ColorGrid">
+                    <ColorGrid />
+                  </Route>
+                  <Route path="/ToDoList">
+                    <ToDoList />
+                  </Route>
+                  <Route path="/Jokes">
+                    <Jokes />
+                  </Route>
+                  <Route path="/Gif">
+                    <Gif />
+                  </Route>
+                  <Route path="/TicTacToe">
+                    <TicTacToe />
+                  </Route>
+                  <Route path="/HangMan">
+                    <HangMan />
+                  </Route>
+                  {/* <Route path="/Jeopardy">
+                    <Jeopardy />
+                  </Route> */}
+                  <Route path="/Radio">
+                    <Radio name="radio-group" id="radio1" order="1" label="Credit card"/>
+                    <Radio name="radio-group" id="radio2" order="2" label="Paypal"/>
+                    <Radio name="radio-group" id="radio3" order="3" label="Check"/>
+                  </Route>
+                  <Route path="/BasicMenu">
+                    <BasicMenu />
+                  </Route>
+                  <Route path="/Tooltip">
+                    <Tooltip text={"I'm ready"} textPosition='bottom'>{'Read me'}</Tooltip>
+                  </Route>
+                  <Route path="/Timer">
+                    <Timer />
+                  </Route>
+                  {/* <Route path="/Slider">
+                    <Slider />
+                  </Route> */}
+                  <Route path="/BirthdayReminder">
+                    <BirthdayReminder />
+                  </Route>
+                  <Route path="/Tours">
+                    <Tours />
+                  </Route>
+                  <Route path="/Reviews">
+                    <Reviews />
+                  </Route>
+                  <Route path="/Accordion">
+                  <div className="accordion-main">
+                    <div className="accordion-container">
+                      <h3>Login Questions</h3>
+                      <section className="info">
+                        {accordionData.map(accordion => {
+                          return (
+                            <Accordion key={accordion.id} question={accordion.title} answer={accordion.info} />
+                          );
+                        })}
+                      </section>
+                    </div>
+                  </div>
+                  </Route>
+                  <Route path="/FoodMenuProject">
+                    <FoodMenuProject />
+                  </Route>
+                </Switch>
 
-              <Switch>
-                <Route path="/FirstComponent">
-                  <FirstComponent />
-                </Route>
-              </Switch>
-              <Switch>
-                <Route path="/SecondComponent">
-                  {/* looks like this doesn't throw an error...could be made dynamically */}
-                  <SecondComponent />
-                </Route>
-              </Switch>
               </div>
             </Router>
           </div>
 
         </section>
       </main>
-      {/*
-      <SecondComponent />
-      <NamedComponent style={style} name="CK" /> */}
 
-      {/* <Tweet
-        username="chickensrock"
-        name="ed"
-        date="6/05/99"
-        message="Gravy!"
-      />
-      <Tweet
-        username="Mucho Dinero"
-        name="eddy"
-        date="7/02/99"
-        message="Jawbreakers, Ed!"
-      />
-      <Tweet
-        username="my_kingdom_for_suncreen"
-        name="edd"
-        date="7/05/99"
-        message="Messy, messy, messy!"
-      /> */}
-
-      {/* <Person
-        name="AgeEight"
-        age="8"
-        hobbies={["running, schooling, eating"]}
-      />
-      <Person
-        name="TooLongOfAName"
-        age="20"
-        hobbies={["thinking, pondering, querying, imagining"]}
-      />
-      <Person name="Bob" age="30" hobbies={["building, fixing it"]} /> */}
-
-      {/* <Pokedex /> */}
-
-      {/* <ColorGrid /> */}
-
-      {/* <ToDoList /> */}
-
-      {/* <LinkApp />... */}
-
-      {/* <Jokes /> */}
-      {/* <Gif /> */}
-
-      {/* <TicTacToe /> */}
-
-      {/* <HangMan /> */}
-    
-      {/* <Jeopardy /> */}
-
-      {/* <Radio name="radio-group" id="radio1" order="1" label="Credit card"/>
-      <Radio name="radio-group" id="radio2" order="2" label="Paypal"/>
-      <Radio name="radio-group" id="radio3" order="3" label="Check"/> */}
-
-      {/* <BasicMenu /> */}
-
-      {/* <Tooltip text={"I'm ready"} textPosition='bottom'>{'Read me'}</Tooltip> */}
-
-      {/* <Timer/> */}
-
-      {/* <Slider initial={1} max={2} onChange={value => console.log(value)}/> */}
-
-      {/* <BirthdayReminder/>*/}
-
-      {/* <Tours /> */}
-
-      {/* <Reviews /> */}
-      
-      {/* <div className="accordion-main">
-        <div className="accordion-container">
-          <h3>Login Questions</h3>
-          <section className="info">
-            {accordionData.map(accordion => {
-              return (
-                <Accordion key={accordion.id} question={accordion.title} answer={accordion.info} />
-              );
-            })}
-          </section>
-        </div>
-      </div> */}
-
-        {/* <FoodMenuProject />*/}
 
         {/* <RouterBasic /> */}
         
