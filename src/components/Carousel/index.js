96e633afa8a9935e78dcbfc2f96383544817af24
ckandola <kkandola22@hotmail.com>
2021-02-19 15:41:17 -0800

    Set up for Carousel component.

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,12 @@
+// from https://youtu.be/a_7Z7C_JCyo?t=8088
+
+import React from 'react';
+import './Carousel.css';
+
+const Carousel = () => {
+    return (
+        <div>Carousel</div>
+    );
+};
+
+export default Carousel;
