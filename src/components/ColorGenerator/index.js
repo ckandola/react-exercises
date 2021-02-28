afcdf38d49bdda458d36beb0f65b64aefcd4b91e
ckandola <kkandola22@hotmail.com>
2021-02-27 17:56:25 -0800

    Add setup for ColorGenerator.

---
+++ src/components/ColorGenerator/index.js
@@ -0,0 +1,14 @@
+// from https://youtu.be/a_7Z7C_JCyo?t=11478
+
+import React from 'react';
+import './ColorGenerator.css';
+
+const ColorGenerator = () => {
+    return (
+        <div>
+            ColorGenerator
+        </div>
+    );
+};
+
+export default ColorGenerator;
