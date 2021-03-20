e80d2fd18940dba5fe268532a93e4a8e6e6de22d
ckandola <kkandola22@hotmail.com>
2021-03-19 17:20:43 -0700

    Add setup for NavBar.

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+// from https://youtu.be/a_7Z7C_JCyo?t=16927
+
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <h1>NavBar</h1>
+    );
+};
+
+export default NavBar;
