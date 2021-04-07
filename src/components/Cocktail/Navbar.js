9a0fa2acb9b416ab4a1eafc4d1b116b67b06877c
ckandola <kkandola22@hotmail.com>
2021-04-07 14:58:42 -0700

    Get Navbar working.

--- src/components/Cocktail/Navbar.js
+++ src/components/Cocktail/Navbar.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import logo from '../../../logo.svg';
+import logo from './logo.svg';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>Navbar</h2>
-        </div>
+        <nav className="cocktail-navbar">
+            <div className="cocktail-nav-center">
+                <Link to="/cocktail_home">
+                    <img src={logo} alt="cocktail logo" className="cocktail-logo"/>
+                </Link>
+
+                <ul className="cocktail-nav-links">
+                    <li>
+                        <Link to="/cocktail_home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/cocktail_about">About</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
--- src/components/Cocktail/index.js
+++ src/components/Cocktail/index.js
@@ -2,12 +2,36 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './Cocktail.css';
 
-// import pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
+import SingleCocktail from './pages/SingleCocktail';
 
 import Navbar from './Navbar';
+
 const CocktailMain = () => {
     return (
-        <div>CocktailMain</div>
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path="/cocktail_home">
+                    <Home />
+                </Route>
+
+                <Route path="/cocktail_about">
+                    <About />
+                </Route>
+                
+                <Route path="*">
+                    <Error />
+                </Route>
+                
+                <Route path="/cocktail/:id">
+                    <SingleCocktail />
+                </Route>
+
+            </Switch>
+        </Router>
     );
 };
 
