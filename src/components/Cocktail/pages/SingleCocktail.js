6e70ec4c14221eebcaa662bd419f08bd0c65ccff
ckandola <kkandola22@hotmail.com>
2021-04-07 14:24:00 -0700

    Complete more of tutorial setup- pages and css.

---
+++ src/components/Cocktail/pages/SingleCocktail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Loading from '../Loading';
+import { useParams, Link } from 'react-router-dom';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+const SingleCocktail = () => {
+    return (
+        <div>
+            <h2>SingleCocktail Page</h2>
+        </div>
+    );
+};
+
+export default SingleCocktail;
