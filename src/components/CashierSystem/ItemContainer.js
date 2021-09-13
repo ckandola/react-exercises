9b9400d76229d5cc22e61d77f0e6655c64939d7a
ckandola <kkandola22@hotmail.com>
2021-09-13 10:08:25 -0700

    Create ItemContainer. Empty for now.

---
+++ src/components/CashierSystem/ItemContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemContainer = () => {
+    return (
+        <section className="pos-itemcontainer">
+            Item Container
+        </section>
+    );
+};
+
+export default ItemContainer;
